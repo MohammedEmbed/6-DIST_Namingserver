3fd4e2956e1ebcb3d084e49174e7628d7b2ef839
Daan Dekoning Krekels <daan.dekoningkrekels@student.ap.be>
2025-03-20 11:39:24 +0100

    Added getHash implementation

--- src/main/java/kaasenwijn/namingserver/service/NameService.java
+++ src/main/java/kaasenwijn/namingserver/service/NameService.java
@@ -2,10 +2,19 @@ package kaasenwijn.namingserver.service;
 
 import org.springframework.stereotype.Service;
 
+import static java.lang.Math.abs;
+
 @Service
 public class NameService {
 
+    /**
+     * Implementation of the hashing function as described in the project explanation.
+     * It has a bound between 0 and 32 768.
+     *
+     * @param name the string value to hash
+     * @return hash of the name as an integer value between 0 and 32 768
+     */
     public Integer getHash(String name){
-    return 0;
+        return (name.hashCode() + Integer.MAX_VALUE * (32768/Integer.MAX_VALUE +abs(Integer.MIN_VALUE)));
     }
 }
