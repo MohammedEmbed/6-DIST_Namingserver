290b80fb43de715873d0639958c990e807770c9d
WarreH <warrehofmans@gmail.com>
2025-03-20 11:31:59 +0100

    singleton repo

--- src/main/java/kaasenwijn/namingserver/repository/IpRepository.java
+++ src/main/java/kaasenwijn/namingserver/repository/IpRepository.java
@@ -1,24 +1,38 @@
 package kaasenwijn.namingserver.repository;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 
-// TODO: make this a singleton + thread safe
 public class IpRepository {
-    private final Map<Integer, String> map = new HashMap<>(); //[Integer, Ip Address] => node
+    private final Map<Integer, String> ipMap = new HashMap<>(); //[Integer, Ip Address] => node
+    private static IpRepository single_instance = null;
 
-    public IpRepository() {
+    @Value("${repository.filename}")
+    private String fileName;
+    private IpRepository()
+    {
         this.readJson();
     }
 
+    private static synchronized IpRepository getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new IpRepository();
+
+        return single_instance;
+    }
+
     public String getIp(int id) {
-        return map.getOrDefault(id, "ip doesn't exist");
+        return this.ipMap.getOrDefault(id, "ip doesn't exist");
 
     }
 
     public void setIp(Integer id, String ip) {
-        this.map.put(id,ip);
+        this.ipMap.put(id,ip);
         this.writeJson();
     }
 
@@ -28,9 +42,14 @@ public class IpRepository {
 
     private void writeJson() {
         // TODO: write hashmap to json
-        JSONObject jsonObject = new JSONObject(map);
+        JSONObject jsonObject = new JSONObject(ipMap);
         String orgJsonData = jsonObject.toString();
+        // Write JSON to file
+        try (FileWriter file = new FileWriter(this.fileName)) {
+            file.write(orgJsonData);
+            System.out.println("JSON file created successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-
 }
