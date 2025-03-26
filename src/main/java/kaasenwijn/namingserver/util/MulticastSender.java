ca4b0c7a6c5147bfac26e2330a2f51d705ee1581
Arvo <cantarvo@gmail.com>
2025-03-26 17:18:15 +0100

    MulticastSender Simplified In this commit I've tried to simplify the multicastsender. It just needs to send its name and ip.

--- src/main/java/kaasenwijn/namingserver/util/MulticastSender.java
+++ src/main/java/kaasenwijn/namingserver/util/MulticastSender.java
@@ -4,13 +4,20 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+import org.json.JSONObject;
+
 public class MulticastSender {
     private static final String MULTICAST_ADDRESS = "230.0.0.0"; // multicast group address
     private static final int PORT = 4446; // port on which to send
 
-    public static void sendMulticastMessage(String message) {
+    public static void sendMulticastMessage(String name, String ip) {
         try (DatagramSocket socket = new DatagramSocket()) { // creates a UDP socket
-            byte[] buf = message.getBytes(); // the message we want to send turned into bytes
+            // Create JSON message with name and ip
+            JSONObject messageObj = new JSONObject();
+            messageObj.put("type", "bootstrap");
+            messageObj.put("name", name);
+            messageObj.put("ip", ip);
+            byte[] buf = messageObj.toString().getBytes(); // the message we want to send turned into bytes
 
             InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
             DatagramPacket packet = new DatagramPacket(buf, buf.length, group, PORT);
