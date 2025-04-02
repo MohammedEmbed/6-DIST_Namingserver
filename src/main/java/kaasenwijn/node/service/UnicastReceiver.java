52f5b04fab92e0c4cd650bb336a1bc28aaedbcb6
Arvo <cantarvo@gmail.com>
2025-04-02 16:23:18 +0200

    OOP principles

--- src/main/java/kaasenwijn/node/service/UnicastReceiver.java
+++ src/main/java/kaasenwijn/node/service/UnicastReceiver.java
@@ -1,5 +1,35 @@
 package kaasenwijn.node.service;
 
-public class UnicastReceiver {
+import kaasenwijn.namingserver.service.NameService;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class UnicastReceiver extends Thread{
+    private final int currentID;
+    private int previousID;
+    private int nextID;
+    NameService nameService;
+
+    public UnicastReceiver(String nodeName) {
+        this.currentID = nameService.getHash(nodeName);
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8081);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            }
+        } catch (IOException e) {
+            System.err.println("Error starting UnicastReceiver: " + e.getMessage());
+        }
+    }
 }
