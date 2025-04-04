7292c189f97ddddf99805560688f2dc0e647d498
Arvo <cantarvo@gmail.com>
2025-04-04 09:52:31 +0200

    Step 2: hash calculated and report to naming server

--- namenode/src/main/java/kaasenwijn/namenode/service/NodeService.java
+++ namenode/src/main/java/kaasenwijn/namenode/service/NodeService.java
@@ -2,6 +2,14 @@ package kaasenwijn.namenode.service;
 
 import kaasenwijn.namenode.repository.NodeRepository;
 import org.springframework.stereotype.Service;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 import java.io.File;
 
@@ -13,7 +21,9 @@ public class NodeService {
         NodeRepository repo = NodeRepository.getInstance();
         repo.setCurrentId(id);
         repo.setSelfIp(ip);
+
         verifyLocalFiles();
+        reportLocalFilesToNamingServer();
     }
     //The same hash function as the Namingserver:
     public static Integer getHash(String name){
@@ -54,6 +64,49 @@ public class NodeService {
         for (File file : files) {
             if (file.isFile()) {
                 System.out.println("- " + file.getName());
+            }
+        }
+    }
+
+    public static void reportLocalFilesToNamingServer() {
+        File folder = new File(System.getProperty("user.dir") + File.separator + "files");
+        if (!folder.exists()) return;
+
+        NodeRepository repo = NodeRepository.getInstance(); //Instantie opvragen
+        String selfIp = repo.getSelfIp(); //Vraag Ip op
+
+        for (File file : folder.listFiles()) { //Lijst alle files van de node op
+            if (!file.isFile()) continue;
+
+            String filename = file.getName();
+            int fileHash = getHash(filename); //Bereken hun hashes
+
+            try {
+                //Probeer via Post request stap 2:  local node has to report that to the
+                //naming server to voldoen. Deze code is wel regelrecht gepikt van stackoverflow. Best eens testen
+
+                URL url = new URL("http://127.0.0.1:8080/api/file/report"); //TODO: Change --> Momenteel localhost voor NamingServer
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setDoOutput(true);
+
+                String json = String.format("{\"filename\":\"%s\", \"hash\":%d, \"ip\":\"%s\"}",
+                        filename, fileHash, selfIp);
+
+                try (OutputStream os = conn.getOutputStream()) {
+                    os.write(json.getBytes());
+                    os.flush();
+                }
+
+                if (conn.getResponseCode() == 200) {
+                    System.out.println("Reported file: " + filename);
+                } else {
+                    System.err.println("Failed to report file: " + filename);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
