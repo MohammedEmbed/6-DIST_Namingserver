5ad7958e481eb432ca14948f9e6cf1748b21a287
Arvo <cantarvo@gmail.com>
2025-04-04 08:34:23 +0200

    Deze code in main naming server was gedelete?
    String containerName = System.getenv("CONTAINER_NAME");
            System.out.println("Host name:"+containerName)

--- nameserver/src/main/java/kaasenwijn/namingserver/NamingserverApplication.java
+++ nameserver/src/main/java/kaasenwijn/namingserver/NamingserverApplication.java
@@ -1,5 +1,7 @@
 package kaasenwijn.namingserver;
 
+import kaasenwijn.namingserver.repository.IpRepository;
+import kaasenwijn.namingserver.service.NameServerMulticastListener;
 import kaasenwijn.namingserver.service.NameService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +15,8 @@ public class NamingserverApplication {
     public static void main(String[] args) throws UnknownHostException {
         String ip = InetAddress.getLocalHost().getHostAddress();
         System.out.println("Node started with IP-address: "+ip);
+        String containerName = System.getenv("CONTAINER_NAME");
+        System.out.println("Host name:"+containerName);
         SpringApplication.run(NamingserverApplication.class, args);
 
         NameService nameService = new NameService(); // or get it from Spring
