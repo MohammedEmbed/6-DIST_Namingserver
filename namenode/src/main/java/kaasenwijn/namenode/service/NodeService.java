8c7d03675ddf15b49bedf917a39094054bb056a5
Arvo <cantarvo@gmail.com>
2025-04-04 09:33:57 +0200

    VerifyLocalFiles node

--- namenode/src/main/java/kaasenwijn/namenode/service/NodeService.java
+++ namenode/src/main/java/kaasenwijn/namenode/service/NodeService.java
@@ -3,6 +3,8 @@ package kaasenwijn.namenode.service;
 import kaasenwijn.namenode.repository.NodeRepository;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+
 @Service
 public class NodeService {
 
@@ -11,6 +13,7 @@ public class NodeService {
         NodeRepository repo = NodeRepository.getInstance();
         repo.setCurrentId(id);
         repo.setSelfIp(ip);
+        verifyLocalFiles();
     }
     //The same hash function as the Namingserver:
     public static Integer getHash(String name){
@@ -36,5 +39,23 @@ public class NodeService {
         }
     }
 
+    public static void verifyLocalFiles(){
+        /*
+        De files voor elke node moeten momenteel bijgehouden worden in het mapje files.
+        de folder (lijn hier onder) kijkt op die map en lijst de files op die erin zetten.
+
+        Dan extra is nog kort alle local files printen (ter info mag eigenlijk weg)
+         */
+        File folder = new File("files");
+        File[] files = folder.listFiles();
+
+        //Print out local files
+        System.out.println("Local files:");
+        for (File file : files) {
+            if (file.isFile()) {
+                System.out.println("- " + file.getName());
+            }
+        }
+    }
 
 }
