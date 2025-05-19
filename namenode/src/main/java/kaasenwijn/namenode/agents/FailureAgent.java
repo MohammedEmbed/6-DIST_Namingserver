05b3ef3beb95a2646849f4d395fa131d90f82fbc
Daan Dekoning Krekels <daan.dekoningkrekels@student.ap.be>
2025-05-19 09:55:38 +0200

    Update FailureAgent structure

--- namenode/src/main/java/kaasenwijn/namenode/agents/FailureAgent.java
+++ namenode/src/main/java/kaasenwijn/namenode/agents/FailureAgent.java
@@ -1,16 +1,8 @@
 package kaasenwijn.namenode.agents;
 
 import jade.core.Agent;
-import jade.core.Location;
 import jade.core.behaviours.Behaviour;
-import kaasenwijn.namenode.model.Neighbor;
 import kaasenwijn.namenode.repository.NodeRepository;
-import kaasenwijn.namenode.util.NodeSender;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
-import java.io.*;
-import java.util.Objects;
 
 
 /**
@@ -20,19 +12,17 @@ import java.util.Objects;
  */
 public class FailureAgent extends Agent {
 /*
-To develop Failure Agent class that implements the Runnable and Serializable interface:
-- The failing node id is added to the constructor,
-- The current node id is added to the constructor (the node that started Failure agent)
-- The run() method will perform:
-    1. Reading the file list of the current node
-    2. If the failing node is the owner of the file, then the file should be transferred to the new owner, but there are two options:
-        1. Option 1: if the new owner doesn’t have a copy of this file already, then the file transfer can be
-            done without any problems. The logs should be updated with a new download location.
-        2. o Option 2: if the file is already stored on the new owner, only the log should be updated
-- Terminate the Failure Agent when it passed all nodes in the ring topology
-    - If the node id is equal to the node id that started the agent -> terminate the Agent
-
-
+    To develop Failure Agent class that implements the Runnable and Serializable interface:
+    - The failing node id is added to the constructor,
+    - The current node id is added to the constructor (the node that started Failure agent)
+    - The run() method will perform:
+        1. Reading the file list of the current node
+        2. If the failing node is the owner of the file, then the file should be transferred to the new owner, but there are two options:
+            1. Option 1: if the new owner doesn’t have a copy of this file already, then the file transfer can be
+                done without any problems. The logs should be updated with a new download location.
+            2. Option 2: if the file is already stored on the new owner, only the log should be updated
+    - Terminate the Failure Agent when it passed all nodes in the ring topology
+        - If the node id is equal to the node id that started the agent -> terminate the Agent
  */
 
     protected int failedNodeId;
--- namenode/src/main/java/kaasenwijn/namenode/agents/FailureBehaviour.java
+++ namenode/src/main/java/kaasenwijn/namenode/agents/FailureBehaviour.java
@@ -1,28 +1,22 @@
 package kaasenwijn.namenode.agents;
 
-
-//Import required Java classes
-import javax.swing.*;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-//Import required Jade classes
 import jade.core.*;
-import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.*;
-import jade.domain.FIPAAgentManagement.Envelope;
 import kaasenwijn.namenode.model.Neighbor;
 import kaasenwijn.namenode.repository.NodeRepository;
 import kaasenwijn.namenode.util.NodeSender;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-
-class FailureBehaviour extends CyclicBehaviour {
+// Based on the DummyAgent
+class FailureBehaviour extends OneShotBehaviour {
     private final static NodeRepository nodeRepository = NodeRepository.getInstance();
 
     FailureBehaviour(Agent a) {
@@ -31,39 +25,8 @@ class FailureBehaviour extends CyclicBehaviour {
 
     public void action() {
         ACLMessage msg = myAgent.receive();
+        FailureAgent agent = (FailureAgent) myAgent;
         if (msg != null) {
-            // ATTENTION!! In order to insert the received message in the queued message list
-            // I cannot simply do it e.g.
-            // ((DummyAgent)myAgent).getGui().queuedMsgListModel.add(0, (Object) new MsgIndication(msg, MsgIndication.INCOMING, new Date()));
-            // WHY?
-            // Because this is not thread safe!!!
-            // In fact, if this operation is executed from this thread while the AWT Event Dispatching
-            // Thread is updating the JList component that shows the queued message list in the DummyAgent
-            // GUI (e.g. because the user has just sent a message), this can cause an inconsistency
-            // between what is shown in the GUI and what the queued message list actually contains.
-            // HOW TO SOLVE THE PROBLEM?
-            // I need to request the AWT Event Dispatching Thread to insert the received message
-            // in the queued message list!
-            // This can be done by using the invokeLater static method of the SwingUtilities class
-            // as below
-
-            SwingUtilities.invokeLater(new EDTRequester((FailureAgent)myAgent, msg));
-        }
-        else {
-            block();
-        }
-    }
-
-    class EDTRequester implements Runnable {
-        FailureAgent agent;
-        ACLMessage msg;
-
-        EDTRequester(FailureAgent a, ACLMessage m) {
-            agent = a;
-            msg = m;
-        }
-
-        public void run() {
             String logFolderPath = "logs_" + nodeRepository.getName(); //TODO: Is this right?
             File logFolder = new File(logFolderPath);
 
@@ -104,11 +67,14 @@ class FailureBehaviour extends CyclicBehaviour {
 
             // Move to the next node
             int nextId = NodeRepository.getInstance().getNextId();
-            Location nextLocation = here(); //TODO
+            Location nextLocation = agent.here(); //TODO
             System.out.println("[FailureAgent] Done here. Migrating to next node: " + nextId);
-            doMove(nextLocation); // This is a stub. We Need JADE mobility container support.
+            agent.doMove(nextLocation); // This is a stub. We Need JADE mobility container support.
 
+        } else {
+            block();
         }
+
     }
 
 }
